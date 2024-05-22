440500d7bcfdbe0befa15140f0a43f30e5b9d852
MariaVitoria Vitória Ribeiro Baptista <maria.ribeirobp@gmail.com>
2024-05-22 16:44:50 -0300

    creating apps

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME1')
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
